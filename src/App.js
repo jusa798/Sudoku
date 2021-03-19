7d1cb4d48b87d250179a2dbd5b63133861373a6c
Jr <richard.beltranjr@gmail.com>
2021-03-19 03:18:48 -0400

    plz work

--- src/App.js
+++ src/App.js
@@ -1,49 +1,42 @@
-import React, {useState} from 'react';
-import './App.css';
-import SudokuBoard from "./Components/SudokuBoard/SudokuBoard"
-import {store, updateIncorrect} from './index'
+import React, { useState } from "react";
+import "./App.css";
+import SudokuBoard from "./Components/SudokuBoard/SudokuBoard";
+import { store, updateIncorrect } from "./index";
 
+function App() {
+  let nullBoard = [
+    [null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null],
 
-function App() {  
-  let nullBoard = 
-  [   [null, null, null,    null, null, null,     null,  null,  null],
-      [null, null, null,    null, null, null,     null,  null,  null],
-      [null, null, null,    null, null, null,     null,  null,  null], 
+    [null, null, null, null, null, null, null, null, null], //Representation of the board
+    [null, null, null, null, null, null, null, null, null], //Starts out as null so that board can be updated
+    [null, null, null, null, null, null, null, null, null], //By State to compare to finished result
 
-      [null, null, null,    null, null, null,     null,  null,  null],      //Representation of the board
-      [null, null, null,    null, null, null,     null,  null,  null],      //Starts out as null so that board can be updated
-      [null, null, null,    null, null, null,     null,  null,  null],      //By State to compare to finished result
-
-      [null, null, null,    null, null, null,     null,  null,  null], 
-      [null, null, null,    null, null, null,     null,  null,  null], 
-      [null, null, null,    null, null, null,     null,  null,  null], 
-];
+    [null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null],
+  ];
 
   let createNewBoard = () => {
-    return <SudokuBoard arrIndex={randomNumberGenerator()} restart={makeNewGame}/>
-}
+    return (
+      <SudokuBoard arrIndex={randomNumberGenerator()} restart={makeNewGame} />
+    );
+  };
 
-
-  let randomNumberGenerator = () => { 
+  let randomNumberGenerator = () => {
     let randomNumber = Math.floor(Math.random() * 9);
-    return randomNumber
-  }
+    return randomNumber;
+  };
   let makeNewGame = () => {
-    store.dispatch(updateIncorrect(nullBoard))
-     
-    setNewGame(createNewBoard())
-  }
+    store.dispatch(updateIncorrect(nullBoard));
 
-  let [newGame, setNewGame] = useState(createNewBoard())
-  
+    setNewGame(createNewBoard());
+  };
 
-  return (
-    <div className="Layout">
-    
-      {newGame}
-              
-    </div>
-  );
+  let [newGame, setNewGame] = useState(createNewBoard());
+
+  return <div className='Layout'>{newGame}</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,81 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Provider} from 'react-redux'
-import  {createStore} from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
+// REDUX
+let nullBoard = [
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
 
-// REDUX 
-let nullBoard = 
-                    [   [null, null, null,    null, null, null,     null,  null,  null],
-                        [null, null, null,    null, null, null,     null,  null,  null],
-                        [null, null, null,    null, null, null,     null,  null,  null], 
+  [null, null, null, null, null, null, null, null, null], //Representation of the board
+  [null, null, null, null, null, null, null, null, null], //Starts out as null so that board can be updated
+  [null, null, null, null, null, null, null, null, null], //By State to compare to finished result
 
-                        [null, null, null,    null, null, null,     null,  null,  null],      //Representation of the board
-                        [null, null, null,    null, null, null,     null,  null,  null],      //Starts out as null so that board can be updated
-                        [null, null, null,    null, null, null,     null,  null,  null],      //By State to compare to finished result
-
-                        [null, null, null,    null, null, null,     null,  null,  null], 
-                        [null, null, null,    null, null, null,     null,  null,  null], 
-                        [null, null, null,    null, null, null,     null,  null,  null], 
-    ];
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+];
 
 //REDUCER
 function boardReducer(board = nullBoard, action) {
   switch (action.type) {
-    case 'INCORRECT':
+    case "INCORRECT":
       //This is assigning a whole new updated board, supplied from the state by the hook setIncorrect in Component <SudokuBoard>
-    return Object.assign({}, board, {
-      board: action.board
-    })
+      return Object.assign({}, board, {
+        board: action.board,
+      });
 
     case "SOLVED":
       return Object.assign({}, board, {
-        board: action.board
-      })
+        board: action.board,
+      });
 
     default:
-      return board
+      return board;
   }
 }
 
 function updateIncorrect(board) {
   return {
     type: "INCORRECT",
-    board: board
-  }
+    board: board,
+  };
 }
 
 function updateSolved(board) {
   return {
     type: "SOLVED",
-    board: board
-  }
+    board: board,
+  };
 }
 
 //STORE
-const store = createStore(boardReducer)
-
+const store = createStore(boardReducer);
 
 ReactDOM.render(
-  
   <Provider store={store}>
-
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
-
   </Provider>,
 
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-export {updateIncorrect, store, updateSolved};
+export { updateIncorrect, store, updateSolved };
